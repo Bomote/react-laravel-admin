68fc54a5308f20476d203399239c2d83eccd9d67
Bomote <bomote.chris@gmail.com>
2023-10-13 15:40:51 +0100

    Added context to main.js, added states to context, added css

--- React/src/components/DefaultLayout.jsx
+++ React/src/components/DefaultLayout.jsx
@@ -1,10 +1,28 @@
-import { Outlet } from "react-router-dom"
+import  {Link, Navigate, Outlet } from "react-router-dom"
+import { useStateContext } from "../context/CreateProvider"
 
 export default function DefaultLayout() {
+  const {user, token} = useStateContext()
+  
+  //redirects to login if user tries to accept pages without loggging in
+  if(!token){
+    return <Navigate to="/login"/>
+  }
   return (
-    <div>
-      
-      <Outlet/>
+    <div id="defaultLayout">
+      <aside>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/users">Users</Link>
+      </aside>
+      <div className="content">
+        <header>
+          <div>Header</div>
+          <div>User info</div>
+        </header>
+        <main>
+          <Outlet/>
+        </main>
+      </div>
     </div>
   )
 }
--- React/src/components/GuestLayout.jsx
+++ React/src/components/GuestLayout.jsx
@@ -1,8 +1,16 @@
-import { Outlet } from "react-router-dom"
+import { Navigate, Outlet } from "react-router-dom"
+import { useStateContext } from "../context/CreateProvider"
 
 export default function GuestLayout() {
+    const {token} = useStateContext()
+
+    //if they're already signed in and trying to sign in they are redirected to homepage
+    if(token){
+        return <Navigate to="/"/>
+    }
   return (
     <div>
+        Guest
         <Outlet/>
     </div>
   )
